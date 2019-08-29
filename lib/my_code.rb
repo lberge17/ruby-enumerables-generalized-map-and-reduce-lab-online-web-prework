f46db96439c8e87cd48166b1074b7aeed52b9f95
Laura Berge <lauraeberge@gmail.com>
2019-08-29 18:06:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(source_array)
+  i = 0
+  newArray = []
+  until i >= source_array.length do
+    newArray.push(source_array[i])
+    i +=1
+  end
+  yield
+  newArray
+end
+
+def reduce(source_array, starting_value=0)
+  i = 0
+  total = starting_value
+  until i >= source_array.length do
+    total += source_array[i]
+    i +=1
+  end
+  return total
+end
